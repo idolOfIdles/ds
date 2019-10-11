8c0d17a2933020e602c1b3aad3f534b5fd8baffd
safayat <safayat@cefalo.com>
2019-10-11 19:07:09 +0600

    update

--- tmdb.py
+++ tmdb.py
@@ -20,6 +20,7 @@ import numpy as np
 import json
 from nltk import flatten
 from itertools import groupby
+import re
 
 train = pd.read_csv('train.csv')
 test  = pd.read_csv('test.csv')
@@ -36,8 +37,28 @@ for i in range(15):
     train['genre_' + sorted_count_genres[i][0]] = train['gnr'].apply(lambda x: 1 if sorted_count_genres[i][0] in x else 0)
 
 
-def toJson(col):
-    return train[col].apply(lambda x: x.replace("'", "\"").replace("\\x",'') if type(x) is str else x).apply(lambda x: json.loads(x) if type(x) is str else [])
-        
+def toJson(str, rx):
+    #p = re.compile('\'name\': \'[^\']+\'') 
+    p = re.compile(rx) 
+    rs = p.findall(str)
+    quotedNames = map(lambda x : x.split(":")[1].strip(), rs)
+    return list(map(lambda x: x[1:len(x)-1], quotedNames))
+    
+def processListStringColumns(col, rx, count):
+    backupColName = 'backup_' + col 
+    sorted_count_map = getSortedCountMap(col,rx)
+    for i in range(count):
+        train[ col+'_'+ sorted_count_map[i][0]] = train[backupColName].apply(lambda x: 1 if sorted_count_map[i][0] in x else 0)
+
+def getSortedCountMap(col, rx):
+    backupColName = 'backup_' + col 
+    train[backupColName] = train[col].apply(lambda x: toJson(x, rx) if type(x) is str else [])
+    sortedList =  sorted(flatten(list(train[backupColName].values))) 
+    countMap = [ (i, len(list(c))) for i,c in groupby(sortedList)]
+    return sorted(countMap, key=lambda x: x[1], reverse=1)
+
+    
+    
+       
 
 
